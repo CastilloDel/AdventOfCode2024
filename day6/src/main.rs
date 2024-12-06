afc258f41b21c3e5fcf3b7a8e8edd9323bbcd435
CastilloDel <delcastillodelarosadaniel@gmail.com>
2024-12-06 21:25:45 +0100

    Day6 part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,8 +1,12 @@
-use std::{collections::HashSet, fs, ops::Index};
+use std::{
+    collections::HashSet,
+    fs,
+    ops::{Index, IndexMut},
+};
 
 type Position = (usize, usize);
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Left,
@@ -10,17 +14,19 @@ enum Direction {
     Right,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum Cell {
     Empty,
     Obstacle,
     Guard,
 }
 
+#[derive(Debug, Clone)]
 struct Map {
     inner: Vec<Vec<Cell>>,
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Guard {
     position: Position,
     direction: Direction,
@@ -64,33 +70,78 @@ impl Index<Position> for Map {
     }
 }
 
+impl IndexMut<Position> for Map {
+    fn index_mut(&mut self, pos: Position) -> &mut Self::Output {
+        &mut self.inner[pos.0][pos.1]
+    }
+}
+
 fn main() {
     let contents = fs::read_to_string("input").unwrap();
     let result = day6_part1(&contents);
     println!("Day6 part 1 result: {result}");
+    let result = day6_part2(&contents);
+    println!("Day6 part 2 result: {result}");
 }
 
 fn day6_part1(input: &str) -> usize {
     let (guard, map) = read_input(input);
-    get_cell_visited_before_leaving(guard, map)
+    let mut cells = get_cells_visited_before_leaving(guard, &map);
+    // Add the original pos if the guard didn't pass through it again
+    cells.insert(guard.position);
+    cells.len()
 }
 
-fn get_cell_visited_before_leaving(mut guard: Guard, map: Map) -> usize {
+fn day6_part2(input: &str) -> usize {
+    let (guard, map) = read_input(input);
+    get_cells_visited_before_leaving(guard, &map)
+        .into_iter()
+        .filter(|&cell| {
+            let mut modified_map = map.clone();
+            modified_map[cell] = Cell::Obstacle;
+            check_loop(guard, modified_map)
+        })
+        .count()
+}
+
+fn check_loop(mut guard: Guard, map: Map) -> bool {
     let mut visited = HashSet::new();
     loop {
         let next_pos = map.get_neighbor_in_direction(guard.position, guard.direction);
-        if let Some(next_pos) = next_pos {
-            if map[next_pos] == Cell::Obstacle {
-                guard.direction = get_next_direction_clockwise(guard.direction);
-                continue;
-            }
-            guard.position = next_pos;
-            visited.insert(next_pos);
+        if let None = next_pos {
+            return false;
+        }
+        let next_pos = next_pos.unwrap();
+
+        if map[next_pos] == Cell::Obstacle {
+            guard.direction = get_next_direction_clockwise(guard.direction);
         } else {
+            guard.position = next_pos;
+        }
+        if visited.contains(&guard) {
+            return true;
+        }
+        visited.insert(guard);
+    }
+}
+
+fn get_cells_visited_before_leaving(mut guard: Guard, map: &Map) -> HashSet<Position> {
+    let mut visited = HashSet::new();
+    loop {
+        let next_pos = map.get_neighbor_in_direction(guard.position, guard.direction);
+        if let None = next_pos {
             break;
         }
+        let next_pos = next_pos.unwrap();
+
+        if map[next_pos] == Cell::Obstacle {
+            guard.direction = get_next_direction_clockwise(guard.direction);
+        } else {
+            guard.position = next_pos;
+            visited.insert(next_pos);
+        }
     }
-    visited.len()
+    visited
 }
 
 fn get_next_direction_clockwise(direction: Direction) -> Direction {
@@ -145,5 +196,19 @@ mod tests {
         let contents = fs::read_to_string("input").unwrap();
         let result = day6_part1(&contents);
         assert_eq!(result, 4656);
+    }
+
+    #[test]
+    fn part2_correct_output_for_test_input() {
+        let contents = fs::read_to_string("test_input").unwrap();
+        let result = day6_part2(&contents);
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn part2_correct_output_for_input() {
+        let contents = fs::read_to_string("input").unwrap();
+        let result = day6_part2(&contents);
+        assert_eq!(result, 1575);
     }
 }
