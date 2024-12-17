60b86aad0c3930e1a3d9fe67eda735bd8455f58d
CastilloDel <delcastillodelarosadaniel@gmail.com>
2024-12-17 19:53:31 +0100

    Day14 part2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -5,12 +5,13 @@ use nom::{
     sequence::{preceded, separated_pair},
     IResult,
 };
-use std::fs;
+use std::{fs, thread::sleep, time::Duration};
 
 fn main() {
     let contents = fs::read_to_string("input").unwrap();
     let result = day14_part1(&contents, (101, 103));
     println!("Day14 part 1 result: {result}");
+    day14_part2(&contents, (101, 103));
 }
 
 fn day14_part1(input: &str, size: Vector) -> usize {
@@ -22,6 +23,26 @@ fn day14_part1(input: &str, size: Vector) -> usize {
     get_robots_per_quadrant(&final_robots, size)
         .iter()
         .product()
+}
+
+fn day14_part2(input: &str, size: Vector) {
+    let (_, mut robots) = read_input(input).unwrap();
+    let expected_robots = robots.len() / 4;
+    for i in 1..10000 {
+        robots = robots
+            .into_iter()
+            .map(|robot| advance_robot(robot, size, 1))
+            .collect::<Vec<Robot>>();
+        let quadrants = get_robots_per_quadrant(&robots, size);
+        if quadrants
+            .iter()
+            .any(|&quadrant| quadrant > expected_robots * 2)
+        {
+            print_robots(&robots, size);
+            println!("{}", i);
+            sleep(Duration::from_millis(400));
+        }
+    }
 }
 
 fn advance_robot(mut robot: Robot, size: Vector, times: i32) -> Robot {
@@ -36,7 +57,7 @@ fn advance_robot(mut robot: Robot, size: Vector, times: i32) -> Robot {
     robot
 }
 
-fn get_robots_per_quadrant(final_robots: &[Robot], size: Vector) -> [usize; 4] {
+fn get_robots_per_quadrant(robots: &[Robot], size: Vector) -> [usize; 4] {
     let x_middle = size.0 / 2;
     let x_separation = size.0 % 2;
     let y_middle = size.1 / 2;
@@ -45,7 +66,7 @@ fn get_robots_per_quadrant(final_robots: &[Robot], size: Vector) -> [usize; 4] {
     let mut second_quadrant = 0;
     let mut third_quadrant = 0;
     let mut fourth_quadrant = 0;
-    for robot in final_robots {
+    for robot in robots {
         if robot.position.0 < x_middle {
             if robot.position.1 < y_middle {
                 first_quadrant += 1;
@@ -66,6 +87,27 @@ fn get_robots_per_quadrant(final_robots: &[Robot], size: Vector) -> [usize; 4] {
         third_quadrant,
         fourth_quadrant,
     ]
+}
+
+fn print_robots(robots: &[Robot], size: (i32, i32)) {
+    let mut matrix = vec![vec![false; size.0 as usize]; size.1 as usize];
+    for robot in robots {
+        matrix[robot.position.1 as usize][robot.position.0 as usize] = true;
+    }
+    for row in matrix {
+        for element in row {
+            if element {
+                print!("#");
+            } else {
+                print!(" ");
+            }
+        }
+        println!("");
+    }
+    for _ in 0..size.0 {
+        print!(".");
+    }
+    println!("");
 }
 
 type Vector = (i32, i32);
